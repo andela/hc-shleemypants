eaff770fe6785fb5bbe082f0b7a835e038dc574d
Marlone Akidiva <marlonea@Marlones-MacBook-Pro.local>
2018-03-27 15:41:22 +0300

    [Feature 155965533] add assertions to test_badge.py

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -1,3 +1,4 @@
+import json
 from django.conf import settings
 from django.core.signing import base64_hmac
 
@@ -14,6 +15,7 @@ class BadgeTestCase(BaseTestCase):
     def test_it_rejects_bad_signature(self):
         r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
         ### Assert the expected response status code
+        self.assertEqual(r.status_code, 400)
 
     def test_it_returns_svg(self):
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
@@ -22,3 +24,7 @@ class BadgeTestCase(BaseTestCase):
 
         r = self.client.get(url)
         ### Assert that the svg is returned
+        self.assertEqual(r.status_code, 200)
+        self.assertIn("image/svg+xml", str(r))
+
+
