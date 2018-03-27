699703a6435a6e86521392e6500699a2be6eb97e
Marlone Akidiva <marlonea@Marlones-MacBook-Pro.local>
2018-03-27 19:35:42 +0300

    [Feature 155965533] improve tests on test_pause.py

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -13,6 +13,7 @@ class PauseTestCase(BaseTestCase):
                              HTTP_X_API_KEY="abc")
 
         ### Assert the expected status code and check's status
+        self.assertEqual(r.status_code, 200)
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -25,3 +26,28 @@ class PauseTestCase(BaseTestCase):
         self.assertEqual(r.status_code, 400)
 
         ### Test that it only allows post requests
+
+    def test_it_only_allows_post_requests(self):
+       
+        check = Check(user=self.alice, status="up")
+        check.save()
+
+        url = "/api/v1/checks/%s/pause" % check.code
+        r = self.client.post(url, "", content_type="application/json",
+                             HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 200)#Returns(200) for put request
+
+        #Returns (405)method not allowed for other requests
+        r = self.client.get(
+            url, content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.put(
+            url, '', content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+        r = self.client.delete(
+            url, content_type='application/json', HTTP_X_API_KEY="abc")
+        self.assertEqual(r.status_code, 405)
+
+
