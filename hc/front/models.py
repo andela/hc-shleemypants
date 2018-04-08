25ba84c8c0d4b94e546c0ba389b16c2929d4ade7
Rodgers-M <mkishagha@gmail.com>
2018-04-08 09:20:16 +0300

    [feat-55965544] : define the FAQs model

--- hc/front/models.py
+++ hc/front/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=300)
+    pub_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=400)
+ 
+    def __str__(self):
+        return self.answer_text
+
